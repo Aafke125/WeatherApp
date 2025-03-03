364c980d6f1a97f3acd9386ec53248c9fd0780d2
Aafke125 <avanderhaven@me.com>
2025-03-03 10:51:08 +0100

    Added JS for the search form

--- src/script.js
+++ src/script.js
@@ -1,1 +1,9 @@
-let 
+function searchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#location");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", searchSubmit);
