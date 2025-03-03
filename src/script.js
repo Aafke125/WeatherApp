e8ab8a2da88db535eb7607cf89be49fcdbc2037b
Aafke125 <avanderhaven@me.com>
2025-03-03 16:27:13 +0100

    Added Axios

--- src/script.js
+++ src/script.js
@@ -7,3 +7,8 @@ function searchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
+
+    let apiKey = "cc05e6fc059ab2a432b067tfa63aoa4b";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    // 
+    axios.get(apiUrl).then();
